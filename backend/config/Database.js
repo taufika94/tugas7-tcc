d0c945dbd550b06eb21221f44efec26b109e07f0
taufika <taufikaa94@gmail.com>
2025-04-20 11:41:30 +0700

    updated

--- backend/config/Database.js
+++ backend/config/Database.js
@@ -1,6 +1,6 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize("RECOVER_YOUR_DATA", "root", "", {
+const db = new Sequelize("notes_tara", "root", "", {
     host: "34.55.111.115", //104.197.117.136
     dialect: "mysql",
 });
