03e3a021c09598ae743310f607f6442ddf486955
taufika <taufikaa94@gmail.com>
2025-04-20 17:18:31 +0700

    updated

--- frontend/src/utils.js
+++ frontend/src/utils.js
@@ -1,1 +1,1 @@
-export const BASE_URL = "http://https://notes-be196-589948883802.us-central1.run.app/notes:3000";
+export const BASE_URL = "http://https://notes-be196-589948883802.us-central1.run.app/";
